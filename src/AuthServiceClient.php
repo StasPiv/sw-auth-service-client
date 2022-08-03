5c39c8646709282260a279b599eace579fd8604c
Stanislav Pivovartsev <stanislav.pivovartsev@prym.com>
2022-08-03 12:10:20 +0300

    fix namespace

--- src/AuthServiceClient.php
+++ src/AuthServiceClient.php
@@ -2,7 +2,7 @@
 
 declare(strict_types = 1);
 
-namespace SmartWaller\AuthClient;
+namespace SmartWallet\AuthClient;
 
 class AuthServiceClient implements AuthServiceClientInterface
 {
--- src/AuthServiceClientInterface.php
+++ src/AuthServiceClientInterface.php
@@ -2,7 +2,7 @@
 
 declare(strict_types = 1);
 
-namespace SmartWaller\AuthClient;
+namespace SmartWallet\AuthClient;
 
 interface AuthServiceClientInterface
 {
